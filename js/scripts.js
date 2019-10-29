fc101a83b4ead3fff505f7bd18dcc2c1385e5e5b
Student <example@example.com>
2019-10-29 10:52:34 -0700

    add functionality to js

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,22 @@ $(document).ready(function() {
 
   var s1 = $("#s1").val();
   console.log("value is: ", s1);
-  
 
-  let vowels = ["a", "e", "i", "o", "u"];
+  let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-
-  // for( var i = 0; i )
-  if (s1 === vowels) {
-    vowels = "-";
-  } else {
-    alert("please type a sentence to survive");
-  }
+  $(s1).map(function(letter) {
+    vowels.map(function(vowel) {
+      if (letter === vowel){
+        s1.replace("-");
+        // FIND A WAY TO MAKE IT A DASH FROM HERE
+      } else {
+        // DO NOTHING
+      }
+    })
+  })
+  //  else {
+  //   alert("please type a sentence to survive");
+  // }
 
 $("#space").text(s1);
 console.log("value is: ", s1);
